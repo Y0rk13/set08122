4355eb0577b288d28be06282bf7308262f80c78f
Y0rk13 <yorkie120705f@gmail.com>
2025-01-30 21:30:20 +0000

    Added initial files
    added two basic hello world files in C and Python.

---
+++ pyhton/basics.py
@@ -0,0 +1,10 @@
+#accessor - x is unchanged
+x = "help me"
+y = x.upper()
+print(y)
+
+#mutator - testList is changed (reversed in order)
+testList = [1, 2, 3]
+testList.reverse()
+print(testList)
+
